30e31b7dc6feb0a4754839097b529a7cd506cd5a
Dinesh Saini <mail.dineshsaini27@gmail.com>
2019-03-14 12:00:38 +0530

    WIP

--- app/Http/Controllers/Api/V1/Invoice/InvoiceController.php
+++ app/Http/Controllers/Api/V1/Invoice/InvoiceController.php
@@ -159,7 +159,13 @@ class InvoiceController extends BaseController
     {
         $order = Order::hash($request->order_hash)->first();
         if ($order) {
-            if ($order->invoice['type'] == 2) {
+            \Log::info("Invoice");
+            \Log::info($order->invoice);
+            \Log::info("TYPE ARRAY");
+            \Log::info($order->invoice['type']);
+            \Log::info("TYPE");
+            \Log::info($order->invoice->type);
+            if ($order->invoice->type == 2) {
 
                 $serviceCharges = $order->invoice->cal_service_charges;
                 $taxes          = $order->invoice->cal_taxes;
