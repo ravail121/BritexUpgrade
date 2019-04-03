4061e87fcf36e36b2d690a6c528df4cdd6dcd5e2
Dinesh Saini <mail.dineshsaini27@gmail.com>
2019-04-03 10:08:53 +0530

    Added package for logs

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
 
         // Barryvdh\Snappy\ServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
