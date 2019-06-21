5f64912cedb646ffa1ae0c36645008a592e38007
Dinesh Saini <mail.dineshsaini27@gmail.com>
2019-06-21 22:15:27 +0530

    WIP

--- app/Model/OrderCouponProduct.php
+++ app/Model/OrderCouponProduct.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace App\model;
+namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
 class OrderCouponProduct extends Model
 {
-    //
+    // table new
+    protected $table = 'order_coupon_product';
+
+    protected $fillable = ['order_coupon_id', 'order_product_type', 'order_product_id', 'amount'];
+
+    public function orderCoupon()
+    {
+        return $this->belongsTo('App\Model\OrderCoupon');
+    }
 }
