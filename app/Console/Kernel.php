ed534bbbeb5a1b05d750fe35c1128788ab270ea1
Dinesh Saini <mail.dineshsaini27@gmail.com>
2019-10-25 18:37:59 +0530

    cron time change for orderdatacontroller

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,8 +38,8 @@ class Kernel extends ConsoleKernel
         
         $schedule->call('App\Http\Controllers\Api\V1\CronJobs\OrderController@order')->everyFiveMinutes();
 
-        $schedule->call('App\Http\Controllers\Api\V1\CronJobs\OrderDataController@order')->hourly();
-        $schedule->call('App\Http\Controllers\Api\V1\CronJobs\OrderDataController@order')->hourlyAt(30);
+        $schedule->call('App\Http\Controllers\Api\V1\CronJobs\OrderDataController@order')->everyMinute();
+        // $schedule->call('App\Http\Controllers\Api\V1\CronJobs\OrderDataController@order')->hourlyAt(30);
     }
 
     /**
