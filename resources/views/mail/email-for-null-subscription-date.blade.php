b3d1b3b66d175e9bf9b11e1e1a80e9e6852ae849
Prajwal Shrestha <prajwal_stha@yahoo.com>
2021-01-29 13:48:28 +0545

    Add changes for email formatting for subscription_start_date null date

--- resources/views/mail/email-for-null-subscription-date.blade.php
+++ resources/views/mail/email-for-null-subscription-date.blade.php
@@ -3,27 +3,12 @@
 
     Please find the list of the customers whose subscription is not closed and have null subscription start date:
     @if($customers)
-        <table>
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Company Name</th>
-                <th>Email</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($customers as $customer)
-                <tr>
-                    <td>{{ $customer['id'] }}</td>
-                    <td>{{ $customer['fname'] }}</td>
-                    <td>{{ $customer['lname'] }}</td>
-                    <td>{{ $customer['company_name'] }}</td>
-                    <td>{{ $customer['email'] }}</td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+        @component('mail::table')
+            | ID                            | First Name                | Last Name                 |  Company Name                     | Email                     |
+            | ----------------------------- |:-------------------------:| :------------------------:| :--------------------------------:|:-------------------------:|
+        @foreach($customers as $customer)
+            | {{ $customer['id'] }}         | {{ $customer['fname'] }}  | {{ $customer['lname'] }}  | {{ $customer['company_name'] }}   | {{ $customer['email'] }}  |
+        @endforeach
+        @endcomponent
     @endif
 @endcomponent
