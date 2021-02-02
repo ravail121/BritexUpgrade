7b451a4c37a1850985159e58f686ef16e02bd4cc
Prajwal Shrestha <prajwal_stha@yahoo.com>
2021-02-02 12:20:58 +0545

    Add minor changes|

--- resources/views/mail/email-for-null-subscription-date.blade.php
+++ resources/views/mail/email-for-null-subscription-date.blade.php
@@ -4,10 +4,10 @@
     Please find the list of the customers whose subscription is not closed and have null subscription start date:
     @if($customers)
         @component('mail::table')
-            | ID                            | First Name                | Last Name                 |  Company Name                     | Email                     |
-            | ----------------------------- |:-------------------------:| :------------------------:| :--------------------------------:|:-------------------------:|
+            | ID                                    | First Name                        | Last Name                         |  Company Name                         | Email                         |
+            | ------------------------------------- |:---------------------------------:| :--------------------------------:| :------------------------------------:|:-----------------------------:|
         @foreach($customers as $customer)
-            | {{ $customer['id'] }}         | {{ $customer['fname'] }}  | {{ $customer['lname'] }}  | {{ $customer['company_name'] }}   | {{ $customer['email'] }}  |
+            | {{ $customer['id'] }}                 | {{ $customer['fname'] }}          | {{ $customer['lname'] }}          | {{ $customer['company_name'] }}       | {{ $customer['email'] }}      |
         @endforeach
         @endcomponent
     @endif
