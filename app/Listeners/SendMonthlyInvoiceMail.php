bb49eb31f237da42c075b3f1d9e6401fc266b18e
Prajwal Shrestha <prajwal_stha@yahoo.com>
2021-02-05 20:39:22 +0545

    Add minor changes

--- app/Listeners/SendMonthlyInvoiceMail.php
+++ app/Listeners/SendMonthlyInvoiceMail.php
@@ -11,7 +11,9 @@ use App\Model\Company;
 use App\Model\EmailTemplate;
 use App\Events\MonthlyInvoice;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\EmailWithAttachment;
 use App\Model\SystemEmailTemplateDynamicField;
+
 
 class SendMonthlyInvoiceMail
 {
@@ -40,42 +42,37 @@ class SendMonthlyInvoiceMail
         // NEED TO RECODE BECAUSE IT WILL GENERATE PREVIOUS INVOICES
         
         $order = Order::where('customer_id', $customer->id)->with('customer')->first();
+        if($order) {
 
-        $invoice = [
-            'start_date' => '2019-01-11',
-            'end_date'   => '2019-02-11',
-            'due_date'   => '2019-02-10',
-            'total_due'  => '23.00',
-            'subtotal'   => '435.00',
-         ];
-         // $invoice = [
-         //    'start_date' => $order->invoice->start_date,
-         //    'end_date'   => $order->invoice->end_date,
-         //    'due_date'   => $order->invoice->due_date,
-         //    'total_due'  => $order->invoice->total_due,
-         //    'subtotal'   => $order->invoice->subtotal,
-         // ];
-        $pdf = PDF::loadView('templates/monthly-invoice', compact('invoice'))->setPaper('letter', 'portrait');
+	        $invoice = [
+		        'start_date' => '2019-01-11',
+		        'end_date'   => '2019-02-11',
+		        'due_date'   => '2019-02-10',
+		        'total_due'  => '23.00',
+		        'subtotal'   => '435.00',
+	        ];
+	        $pdf     = PDF::loadView( 'templates/monthly-invoice', compact( 'invoice' ) )->setPaper( 'letter', 'portrait' );
 
-        $emailTemplates = EmailTemplate::where('company_id', $this->order->company_id)->where('code', 'monthly-invoice')->get();
+	        $emailTemplates = EmailTemplate::where( 'company_id', $this->order->company_id )->where( 'code', 'monthly-invoice' )->get();
 
-        $templateVales  = SystemEmailTemplateDynamicField::where('code', 'one-time-invoice')->get()->toArray();
+	        $templateVales = SystemEmailTemplateDynamicField::where( 'code', 'one-time-invoice' )->get()->toArray();
 
-        $note = 'Invoice Link '.route('api.invoice.get').'?order_hash='.$this->order->hash;
+	        $note = 'Invoice Link ' . route( 'api.invoice.get' ) . '?order_hash=' . $this->order->hash;
 
-        foreach ($emailTemplates as $key => $emailTemplate) {
-            if(filter_var($emailTemplate->to, FILTER_VALIDATE_EMAIL)){
-                $email = $emailTemplate->to;
-            }else{
-                $email = $customer->email;
-            }
-	        $configurationSet = $this->setMailConfiguration($order);
+	        foreach ( $emailTemplates as $key => $emailTemplate ) {
+		        if ( filter_var( $emailTemplate->to, FILTER_VALIDATE_EMAIL ) ) {
+			        $email = $emailTemplate->to;
+		        } else {
+			        $email = $customer->email;
+		        }
+		        $configurationSet = $this->setMailConfiguration( $order );
 
-	        if ($configurationSet) {
-		        return false;
+		        if ( $configurationSet ) {
+			        return false;
+		        }
+		        Notification::route( 'mail', $email )->notify( new EmailWithAttachment( $order, $pdf, $emailTemplate, $this->order->customer->business_verification_id, $templateVales, $note ) );
 	        }
-            Notification::route('mail', $email)->notify(new EmailWithAttachment($order, $pdf, $emailTemplate, $this->order->customer->business_verification_id, $templateVales, $note));
-        }          
+        }
     }
 
 
@@ -89,15 +86,15 @@ class SendMonthlyInvoiceMail
     {
         $company = Company::find($order->company_id);
         $config = [
-            'driver'   => $company->smtp_driver,
-            'host'     => $company->smtp_host,
-            'port'     => $company->smtp_port,
-            'username' => $company->smtp_username,
-            'password' => $company->smtp_password,
-            'encryption' => $company->smtp_encryption,
+            'driver'        => $company->smtp_driver,
+            'host'          => $company->smtp_host,
+            'port'          => $company->smtp_port,
+            'username'      => $company->smtp_username,
+            'password'      => $company->smtp_password,
+            'encryption'    => $company->smtp_encryption,
         ];
 
-        Config::set('mail',$config);
+        Config::set('mail', $config);
         return false;
     }
 
