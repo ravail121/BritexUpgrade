789136b5ab927a477c65c2592854537834fdf7e9
Prajwal Shrestha <prajwal_stha@yahoo.com>
2021-12-29 21:50:57 +0545

    Add minor changes

--- app/Http/Controllers/Api/V1/Traits/InvoiceTrait.php
+++ app/Http/Controllers/Api/V1/Traits/InvoiceTrait.php
@@ -809,7 +809,7 @@ trait InvoiceTrait
 				'',
 				'',
 				'Total',
-				$csvData[ 'invoice' ]->cal_subtotal ? '$ ' . number_format( $csvData[ 'invoice' ]->cal_subtotal, 2 ) : '$ 0.00'
+				$csvData[ 'invoice' ]->subtotal ? '$ ' . number_format( $csvData[ 'invoice' ]->subtotal, 2 ) : '$ 0.00'
 			];
 		}
 		return $csvRows;
