0edd4606243007e4f4aa9f5830bf97634acf3019
Prajwal Shrestha <prajwal_stha@yahoo.com>
2022-03-23 18:26:10 +0545

    Add minor changes

--- app/Events/SubscriptionForReactivation.php
+++ app/Events/SubscriptionForReactivation.php
@@ -2,17 +2,13 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class SubscriptionForReactivation
 {
-
 	/**
 	 * @var
 	 */
