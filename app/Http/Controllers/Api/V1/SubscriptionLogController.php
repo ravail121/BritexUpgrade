c970525f6a56515afdc893fa31121a6288e6ed10
Prajwal Shrestha <prajwal_stha@yahoo.com>
2022-06-25 10:46:30 +0545

    Add minor changes

--- app/Http/Controllers/Api/V1/SubscriptionLogController.php
+++ app/Http/Controllers/Api/V1/SubscriptionLogController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Controllers\Api\V1;
 
+
+use Carbon\Carbon;
 use App\Helpers\Log;
 use Illuminate\Http\Request;
 use App\Model\SubscriptionLog;
@@ -13,18 +15,27 @@ class SubscriptionLogController extends BaseController
 {
 	public function store(Request $request)
 	{
+		$request->merge( [
+			'company_id' => $request->get('company')->id,
+		]);
+
+		if(!$request->has('date')){
+			$request->merge( [
+				'date' => Carbon::now()->toDateTimeString()
+			]);
+		}
 		try {
 			$this->validate($request, [
-				'date'          => 'required',
-				'category'      => 'required',
-				'product_id'    => 'required',
-				'description'   => 'required',
-				'old_product'   => 'required',
-				'new_product'   => 'required'
+				'customer_id'       => 'required',
+				'subscription_id'   => 'required',
+				'product_id'        => 'required',
+				'old_product'       => 'required',
+				'new_product'       => 'required'
 			]);
+
 			$subscription = SubscriptionLog::create($request->all());
 			$response = [
-				'status' => 'error',
+				'status' => 'success',
 				'data'   => $subscription
 			];
 			return $this->respond( $response );
