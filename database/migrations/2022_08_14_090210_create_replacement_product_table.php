93eaa19f22d79d57f8d5c6941668bd20c1b95b54
Prajwal Shrestha <prajwal_stha@yahoo.com>
2022-08-15 07:20:32 +0545

    WIP

---
+++ database/migrations/2022_08_14_090210_create_replacement_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReplacementProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replacement_products', function (Blueprint $table) {
+            $table->increments('id');
+	        $table->unsignedInteger('company_id');
+	        $table->foreign('company_id')->references('id')->on('company');
+	        $table->integer('product_id')->nullable();
+	        $table->text('product_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replacement_products');
+    }
+}
---
+++ database/migrations/2022_08_14_091024_add_subscription_id_to_customer_standalone_sim_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSubscriptionIdToCustomerStandaloneSimTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	    Schema::table('customer_standalone_sim', function (Blueprint $table) {
+		    $table->unsignedInteger( 'subscription_id' )
+		          ->nullable()
+		          ->comment('Subscription ID for the sim');
+		    $table->foreign('subscription_id')
+		          ->references('id')->on('subscription')
+		          ->onUpdate('cascade')
+		          ->onDelete('cascade');
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    Schema::table('customer_standalone_sim', function (Blueprint $table) {
+		    $table->dropForeign('customer_standalone_sim_subscription_id_foreign');
+		    $table->dropColumn( [
+			    'subscription_id'
+		    ] );
+	    });
+    }
+}
