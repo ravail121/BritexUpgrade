35d4d5100be6bb375d72b966533a5ea0fd1c314d
Prajwal Shrestha <prajwal_stha@yahoo.com>
2022-12-20 19:55:23 +0545

    Add changes for pulling unique sim

--- app/Http/Controllers/Api/V1/BulkOrder/CheckoutController.php
+++ app/Http/Controllers/Api/V1/BulkOrder/CheckoutController.php
@@ -305,7 +305,6 @@ class CheckoutController extends BaseController implements ConstantInterface
 			$requestCompany = $request->get('company');
 
 			$customer = Customer::where('company_id', $requestCompany->id)->where('id', $request->get('customer_id'))->first();
-			$perPage = $request->has('per_page') ? (int) $request->get('per_page') : 25;
 
 			$orderSims = CustomerStandaloneSim::where([
 				['customer_id', $customer->id],
@@ -317,7 +316,7 @@ class CheckoutController extends BaseController implements ConstantInterface
 							['carrier_id', 5]
 						]
 					);
-				})->shipping()->with('sim')->paginate($perPage);
+				})->shipping()->distinct('sim_id')->with('sim')->get();
 
 			return $this->respond($orderSims);
 
