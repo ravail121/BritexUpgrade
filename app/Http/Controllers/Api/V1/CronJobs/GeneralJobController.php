b3a6b40b6a8f82a410e3e4997855f5f2bf66304f
ravail121 <rvlirshad@gmail.com>
2023-02-02 15:02:42 +0500

    cron job run fix

--- app/Http/Controllers/Api/V1/CronJobs/GeneralJobController.php
+++ app/Http/Controllers/Api/V1/CronJobs/GeneralJobController.php
@@ -2,23 +2,12 @@
 
 namespace App\Http\Controllers\Api\V1\CronJobs;
 
-use App\CronJobRunStatus;
 use App\Http\Controllers\Controller;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class GeneralJobController extends Controller
 {
     public function generalCronJob(Request $request){
-        $cronStatus = CronJobRunStatus::where('date_stamp',Carbon::today())->first();
-        if($cronStatus){
-            return response(['message' => 'Runned']);
-        }else{
-            $cronJobStatus = new CronJobRunStatus();
-            $cronJobStatus->status = true;
-            $cronJobStatus->date_stamp = Carbon::today();
-            $cronJobStatus->save();
-            
             app('App\Http\Controllers\Api\V1\CronJobs\UpdateController')->checkUpdates($request);
         
             app('App\Http\Controllers\Api\V1\CronJobs\MonthlyInvoiceController')->generateMonthlyInvoice($request);
@@ -35,6 +24,5 @@ class GeneralJobController extends Controller
     
     
             return response(['message' => 'Successfull']);
-        }
     }
 }
